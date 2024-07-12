155337cdb6de0949d9b503f7b04d2108d4cc2d5f
Foitelija <foitelija777@gmail.com>
2024-07-12 10:10:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZxingBarcodeReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
